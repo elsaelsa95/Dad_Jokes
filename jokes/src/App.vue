c0cd3380992d27ff09f547bcaba5bca6a6dabc98
elsaelsa95 <elsa.lustiana95@gmail.com>
2022-12-24 11:15:09 +0700

    jokes

---
+++ jokes/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+import { useJokesStore } from "@/stores/jokes.js";
+import { mapState, mapActions } from "pinia";
+
+export default {
+  methods: {
+    ...mapActions(useJokesStore, ["getJokes"]),
+  },
+  computed: {
+    ...mapState(useJokesStore, ["setup", "punchline"]),
+  },
+};
+</script>
+
+<template>
+  <section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+      <div class="container">
+        <h1 style="text-align: center">{{ setup }}</h1>
+        <br>
+        <br>
+        <h1 style="text-align: center">{{ punchline }}</h1>
+      </div>
+      <button class="button" type="submit" class="btn btn-danger" @click.prevent="getJokes">Reload</button>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.container {
+  background-color: lightblue;
+  opacity: 75%;
+  margin-top: 10vh;
+  margin-right: 10vh;
+  padding: 20px;
+  border-radius: 30px;
+}
+.button {
+  
+}
+
+</style>
---
+++ jokes/src/stores/jokes.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+
+export const useJokesStore = defineStore({
+  id: "jokes",
+  state: () => ({
+    setup: "",
+    punchline: "",
+    url: "https://official-joke-api.appspot.com/random_joke",
+  }),
+
+  actions: {
+    async getJokes() {
+      try {
+        const result = await axios({
+          method: "get",
+          url: "https://official-joke-api.appspot.com/random_joke",
+        });
+        this.setup = result.data.setup;
+        this.punchline = result.data.punchline
+        console.log(result)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+  },
+});
